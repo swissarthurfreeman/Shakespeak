29528f6da55d2a1efb7ca242b29d1ba5853380ad
A. Freeman <swissarthurfreeman@gmail.com>
2023-12-09 14:36:03 +0100

    Add basic transformer test

--- test/Transformer_test.py
+++ test/Transformer_test.py
@@ -1,6 +1,7 @@
 import torch
 import unittest
 import torch.nn as nn
+from torch import Tensor
 from modules.Transformer import Transformer
 
 
@@ -8,15 +9,24 @@ class TestTransformer(unittest.TestCase):
     def test_Transformer(self):
         print("[TEST CASE] Running test_Transformer...")
         
-        B, N, d = 20, 128, 768       # note, with this test d, d_k, d_v MUST REMAIN FIXED
+        B, N, d = 20, 128, 768
         h, d_k, d_v = 8, 64, 32
         V, d_ff, L = 1000, 2048, 12
         
         E = nn.Embedding(V, d)
         t = Transformer(L, B, N, h, d, d_k, d_v, d_ff, V, E)
-        X = torch.rand(size=(B, N, d))
+        X = torch.rand(size=(B, N, d)) * 5456   # test stability
 
-        Y = t(X)
+        Y: Tensor = t(X)
+        # test size is correct
+        self.assertListEqual(list(Y.size()), [B, N, V])
+
+        # test last dimension is indeed a probability distribution
+        self.assertAlmostEqual(Y[5, 60, :].sum().item(), 1, 6)
+        self.assertAlmostEqual(Y[7, 30, :].sum().item(), 1, 6)
+        self.assertAlmostEqual(Y[10, 47, :].sum().item(), 1, 6)
+        self.assertAlmostEqual(Y[12, 104, :].sum().item(), 1, 6)
+        
         
         print("[TEST CASE] test_Transformer Successful...")
 
