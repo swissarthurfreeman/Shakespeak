231370ab7c56caff16ae6cded1be2897fc994764
Yvan Pannatier <yvan.pannatier@hevs.ch>
2023-12-11 23:17:00 +0100

    feat: add first version of learning rate decay

--- model_simple.py
+++ model_simple.py
@@ -1,11 +1,13 @@
 import math
+
 import torch
 import torch.nn as nn
-from torch import Tensor
 import torch.nn.functional as F
 import torch.optim as optim
-from torch.utils.data import Dataset        
+from torch import Tensor
+from torch.utils.data import Dataset
 from torch.utils.data.dataloader import DataLoader
+
 
 class CharDataSet(Dataset):
     """
@@ -15,13 +17,16 @@ class CharDataSet(Dataset):
     yields a tuple with the i-th sliding window 
     and the i+1-th window on the data.   
     """
+
     def __init__(self, N_tokens, data):
-        self.N_tokens = N_tokens        # CAREFUL ! Top bound is not included ! 
+        self.N_tokens = N_tokens        # CAREFUL ! Top bound is not included !
         self.raw_data: str = data
         self.vocabulary: list[str] = sorted(list(set(data)))
         self.vocabulary_size: int = len(self.vocabulary)
-        self.encoder: dict[str, int] = {ch: i for i, ch in enumerate(self.vocabulary)}
-        self.decoder: dict[int, str] = {i: ch for i, ch in enumerate(self.vocabulary)}
+        self.encoder: dict[str, int] = {
+            ch: i for i, ch in enumerate(self.vocabulary)}
+        self.decoder: dict[int, str] = {
+            i: ch for i, ch in enumerate(self.vocabulary)}
 
     def get_vocab_size(self):
         return self.vocabulary_size
@@ -44,15 +49,18 @@ class CharDataSet(Dataset):
 
     def encode(self, text: str) -> Tensor:
         """Map string of characters to vector of indexes."""
-        idx = torch.zeros(size=(1, len(text)), dtype=torch.float32)      # BUG : before we were using self.N_tokens, but this is not expected behavior.
+        idx = torch.zeros(size=(1, len(
+            text)), dtype=torch.float32)      # BUG : before we were using self.N_tokens, but this is not expected behavior.
         for i, char in enumerate(text):
             idx[0, i] = self.encoder[char]
         return idx
 
     def decode(self, idx: Tensor) -> str:
         """Decode list of character token indexes as string."""
-        chars = [self.decoder[i] for i in idx.tolist()]         # why was there an if i != 0 in the list ?  
-        return ''.join(chars)                                   # this made decoding of spaces impossible 
+        chars = [self.decoder[i] for i in idx.tolist(
+        )]         # why was there an if i != 0 in the list ?
+        # this made decoding of spaces impossible
+        return ''.join(chars)
 
 
 def load_data(filename):
@@ -64,7 +72,7 @@ def load_data(filename):
 def getLoaderDataset(N, B, path):
     raw = load_data(path)
     tokenized_data = CharDataSet(N, raw)
-    
+
     data_loader = DataLoader(
         tokenized_data,
         shuffle=True,
@@ -74,6 +82,7 @@ def getLoaderDataset(N, B, path):
     )
 
     return data_loader, tokenized_data
+
 
 class WPE(torch.nn.Module):
     def __init__(self, d):
@@ -88,6 +97,7 @@ class WPE(torch.nn.Module):
         torch.sin_(angles[:, :, 0::2])  # Sinus of even elements
         torch.cos_(angles[:, :, 1::2])  # Cosinus of odd elements
         return angles
+
 
 class QKVAttention(nn.Module):
     def __init__(
@@ -134,8 +144,9 @@ class QKVAttention(nn.Module):
 class Block(nn.Module):
     def __init__(self, h, d, d_ff):
         super().__init__()
-        
-        self.CausalSelfAttn = QKVAttention(dim_in=d, dim_qk=d//h, dim_v=d//h, nb_heads=h, causal=True, attention_dropout=0)
+
+        self.CausalSelfAttn = QKVAttention(
+            dim_in=d, dim_qk=d//h, dim_v=d//h, nb_heads=h, causal=True, attention_dropout=0)
         self.W1 = nn.Linear(d, d_ff)
         self.W2 = nn.Linear(d_ff, d)
 
@@ -146,6 +157,7 @@ class Block(nn.Module):
         X = X + self.CausalSelfAttn(self.LayerNorm1(X))
         out = X + self.W2(F.relu(self.W1(self.LayerNorm2(X))))
         return out
+
 
 class GPT(nn.Module):
     def __init__(self, B, L, d, d_ff, N, h, V):
@@ -162,11 +174,11 @@ class GPT(nn.Module):
 
         for _ in range(L):
             self.blocks.append(Block(h, d, d_ff))
-        
+
         self.Dropout = nn.Dropout(0.2)
         self.Final_LayerNorm = nn.LayerNorm(d)
         self.LM_Head = nn.Linear(d, V)
-    
+
     def forward(self, X):
         X = X.long().to('cuda')
         tok_emb = self.WTE(X)
@@ -182,37 +194,57 @@ class GPT(nn.Module):
         return X
 
 
-
 @torch.no_grad()
 def generate(model, idx, max_new_tokens):
     input = idx.to('cuda')
     for k in range(max_new_tokens):
-        logits = model(input) 
-        
-        #char_id = torch.multinomial(logits[0, -1, :], num_samples=1).item()
-        
-        char_id = torch.distributions.categorical.Categorical(logits=logits[0, -1, :]).sample()
+        logits = model(input)
+
+        # char_id = torch.multinomial(logits[0, -1, :], num_samples=1).item()
+
+        char_id = torch.distributions.categorical.Categorical(
+            logits=logits[0, -1, :]).sample()
 
         new_c = torch.tensor(char_id).reshape(shape=(1, 1))
-        input = torch.concat( (input, new_c), dim=-1 )
+        input = torch.concat((input, new_c), dim=-1)
     return input.flatten()
 
 
-def train_model(L, B, N, d, h): 
-    data_loader, tokenized_data = getLoaderDataset(N, B, "./shakespear_corpus.txt")
+def calculate_learning_rate(iteration: int):
+    if iteration < n_warmup_iterations:
+        return learning_rate * iteration / n_warmup_iterations
+    if iteration > learning_rate_decay_iterations:
+        return min_learning_rate
+    decay_ratio = (iteration - n_warmup_iterations) / \
+        (learning_rate_decay_iterations - n_warmup_iterations)
+    coefficient = 0.5 * (1.0 + math.cos(math.pi * decay_ratio))
+    return min_learning_rate + coefficient * (learning_rate - min_learning_rate)
+
+
+def train_model(L, B, N, d, h, learning_rate):
+    data_loader, tokenized_data = getLoaderDataset(
+        N, B, "./datasets/shakespear_corpus.txt")
     losses = []
-    model = GPT(B, L, d, 3 * d, N, h, tokenized_data.get_vocab_size()).to('cuda')
+    model = GPT(B, L, d, 3 * d, N, h,
+                tokenized_data.get_vocab_size()).to('cuda')
     model.train()
 
     criterion = nn.CrossEntropyLoss(reduction='mean').to('cuda')
-    optimizer = optim.Adam(model.parameters(), lr=1e-4, betas=(0.9, 0.99), eps=10e-9)
-    
-    #inputs, targets = next(iter(data_loader))
+    optimizer = optim.Adam(
+        model.parameters(), lr=learning_rate, betas=(0.9, 0.99), eps=10e-9)
+
+    # inputs, targets = next(iter(data_loader))
     for batch_idx, (inputs, targets) in enumerate(data_loader):
-    #for batch_idx in range(1000):
+        # for batch_idx in range(1000):
+
+        lr = calculate_learning_rate(
+            batch_idx) if use_lr_decay else learning_rate
+        print(lr)
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = lr
         optimizer.zero_grad()
 
-        if batch_idx == 500:
+        if batch_idx == 100:
             break
 
         logits: Tensor = model(inputs)
@@ -228,19 +260,22 @@ def train_model(L, B, N, d, h):
         print(f"batch {batch_idx}, Loss : {loss.item()}")
 
         if batch_idx % 100 == 0:
-          torch.save(model.state_dict(), f"model_{batch_idx}.pt")
+            torch.save(model.state_dict(), f"model_{batch_idx}.pt")
 
     return model, losses
 
-import matplotlib.pyplot as plt
-    
+
 if __name__ == '__main__':
     B = 64
-    N = 256 # context of up to 256 previous characters
+    N = 256  # context of up to 256 previous characters
     L = 6
     h = 6
     d = 384
-
+    learning_rate = 1e-4
+    n_warmup_iterations = 10
+    learning_rate_decay_iterations = 100
+    min_learning_rate = 1e-5
+    use_lr_decay = True
     # Directorz unique pour chaque run
     """
     # Saving
@@ -256,8 +291,9 @@ if __name__ == '__main__':
     model.load_state_dict(ckpt["model"])
     # opt
     """
-    model, losses = train_model(L, B, N, d, h)
+    model, losses = train_model(L, B, N, d, h, learning_rate)
 
-    plt.plot(range(len(losses)), losses)
-    loader, tokenized_data  = getLoaderDataset(N, B, "./shakespear_corpus.txt")
+    # plt.plot(range(len(losses)), losses)
+    loader, tokenized_data = getLoaderDataset(
+        N, B, "./datasets/shakespear_corpus.txt")
     print(tokenized_data.decode(generate(model, tokenized_data.encode("Oh"), 200)))
