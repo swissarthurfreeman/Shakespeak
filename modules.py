1a72cfccc2ba0c703fc6c39cbd5c13aa5f1071cd
Yvan Pannatier <yvan.pannatier@hevs.ch>
2023-12-12 15:02:36 +0100

    fix device error

--- modules.py
+++ modules.py
@@ -1,8 +1,9 @@
 import math
+
 import torch
 import torch.nn as nn
-from torch import Tensor
 import torch.nn.functional as F
+from torch import Tensor
 
 
 class WPE(torch.nn.Module):
@@ -23,9 +24,10 @@ class WPE(torch.nn.Module):
 class Block(nn.Module):
     def __init__(self, h, d, d_ff):
         super().__init__()
-        
-        self.CausalSelfAttn = CausalSelfAttention(d, d_qk=d//h, d_v=d//h, h=h, p=0.2)
-        #self.CausalSelfAttn = QKVAttention(d, dim_qk=d//h, dim_v=d//h, nb_heads=h, causal=True, attention_dropout=0)
+
+        self.CausalSelfAttn = CausalSelfAttention(
+            d, d_qk=d//h, d_v=d//h, h=h, p=0.2)
+        # self.CausalSelfAttn = QKVAttention(d, dim_qk=d//h, dim_v=d//h, nb_heads=h, causal=True, attention_dropout=0)
         self.W1 = nn.Linear(d, d_ff)
         self.W2 = nn.Linear(d_ff, d)
 
@@ -44,7 +46,7 @@ class CausalSelfAttention(nn.Module):
         self.h = h
         self.d_qk = d_qk
         self.d_v = d_v
-        
+
         self.W_Q = nn.Linear(in_features=d, out_features=h*d_qk)
         self.W_K = nn.Linear(in_features=d, out_features=h*d_qk)
         self.W_V = nn.Linear(in_features=d, out_features=h*d_v)
@@ -52,10 +54,11 @@ class CausalSelfAttention(nn.Module):
 
     def forward(self, X: Tensor):          # assume input is Z x B x N x d or N x d
 
-        Q: Tensor = self.W_Q(X)            # B x N x h*d_qkv -> B x N x h x d_qkv
+        # B x N x h*d_qkv -> B x N x h x d_qkv
+        Q: Tensor = self.W_Q(X)
         Q = Q.reshape(shape=(X.size(0), X.size(-2), self.h, self.d_qk))
         Q = Q.transpose(dim0=1, dim1=2).contiguous()
-        
+
         K: Tensor = self.W_K(X)
         K = K.reshape(shape=(X.size(0), X.size(-2), self.h, self.d_qk))
         K = K.transpose(dim0=1, dim1=2).contiguous()
@@ -64,17 +67,20 @@ class CausalSelfAttention(nn.Module):
         V: Tensor = V.reshape(shape=(X.size(0), X.size(-2), self.h, self.d_v))
         V = V.transpose(dim0=1, dim1=2).contiguous()
         # UPDATE HERE IF CUDA SUPPORTS F.scaled_dot_product_attention
-        AV: Tensor = scaled_dot_product_attention(query=Q, key=K, value=V, is_causal=True) # dim B x h x N x d_v
+        AV: Tensor = scaled_dot_product_attention(
+            query=Q, key=K, value=V, is_causal=True)  # dim B x h x N x d_v
         AV = torch.transpose(AV, dim0=1, dim1=2)    # B x N x h x d_v
-        AV = torch.reshape(AV, shape=(-1, X.size(-2), self.h * self.d_v))   # B x N x h*d_v, head concat
+        # B x N x h*d_v, head concat
+        AV = torch.reshape(AV, shape=(-1, X.size(-2), self.h * self.d_v))
         SA_out = self.W_O(AV)
         return SA_out
+
 
 def scaled_dot_product_attention(query, key, value, attn_mask=None, dropout_p=0.0, is_causal=True, scale=None) -> torch.Tensor:
     # see https://pytorch.org/docs/stable/generated/torch.nn.functional.scaled_dot_product_attention.html
     L, S = query.size(-2), key.size(-2)
     scale_factor = 1 / math.sqrt(query.size(-1)) if scale is None else scale
-    attn_bias = torch.zeros(L, S, dtype=query.dtype)
+    attn_bias = torch.zeros(L, S, dtype=query.dtype).to(query.device)
     if is_causal:
         assert attn_mask is None
         temp_mask = torch.ones(L, S, dtype=torch.bool).tril(diagonal=0)
